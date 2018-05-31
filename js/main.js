0bb28c446f0389df8a2503c2046011c53008a3fc
Chad Russell <chadr76@gmail.com>
2018-05-31 16:35:28 -0700

    -email test

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+function pageLoaded() {
+  console.log("Page loaded...");
+
+  setDynamicWidthOfChildrenDiv('center_slider_div');
+  setDynamicWidthOfChildrenDiv('main_slider_div');
+  setDynamicWidthOfChildrenDiv('second_slider_div');
+
+  closeIntro();
+
+  Email.send("chad@btczero.io",
+    "chad@btczero.io",
+    "This is a subject",
+    "this is the body",
+    {token: "90d093d2-7a9a-4470-bf48-90c02928574e"});
+
+}
